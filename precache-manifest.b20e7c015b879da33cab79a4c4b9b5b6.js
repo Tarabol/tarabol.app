a95a0398790fca607e83f0f83062901998436ff2
Anh T. Dang <dangtrunganh@gmail.com>
2021-12-18 17:50:28 +0700

    Updates

---
+++ precache-manifest.b20e7c015b879da33cab79a4c4b9b5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639790aa6bdc67689da0082d1233e850",
+    "url": "/tarabol.app/index.html"
+  },
+  {
+    "revision": "153d040afcbc0ec15a3a",
+    "url": "/tarabol.app/static/css/main.4b891cdb.chunk.css"
+  },
+  {
+    "revision": "ea9f62a1c6fc22b1cb87",
+    "url": "/tarabol.app/static/js/2.2599e598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarabol.app/static/js/2.2599e598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153d040afcbc0ec15a3a",
+    "url": "/tarabol.app/static/js/main.c7d25016.chunk.js"
+  },
+  {
+    "revision": "03194d028c87bc999ab1",
+    "url": "/tarabol.app/static/js/runtime-main.d627400b.js"
+  }
+]);
