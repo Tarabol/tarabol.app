c16929613783938ca82ffd2797adcd746d75b07b
Anh T. Dang <dangtrunganh@gmail.com>
2021-12-18 17:52:15 +0700

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./src/**/*.js', './public/index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    inset: {
+      '1/2': '50%',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
